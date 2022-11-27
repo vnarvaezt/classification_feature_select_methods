a87ebde95e7a73ac74c6744c95a8e368036893b2
vnarvaezt <vnarvaezt@gmail.com>
2022-11-27 10:24:43 +0100

    added features to keep

--- main.py
+++ main.py
@@ -15,13 +15,21 @@ if __name__ == '__main__':
     # X_prepro = prepro.run_preprocessing(
     #     all_x.copy(deep=True),
     #     do_save=True,
-    #     feat_to_keep=["CIVILIAN_LABOR_FORCE_2019"],
+    #     feat_to_keep=["CIVILIAN_LABOR_FORCE_2019",
+    #     'HIGH_SCHOOL_DIPLOMA_ONLY__2015_19",
+    #     'LESS_THAN_A_HIGH_SCHOOL_DIPLOMA__2015_19',
+    #     'SOME_COLLEGE_OR_ASSOCIATE_S_DEGREE__2015_19,
+    #     'PERCENT_OF_ADULTS_WITH_A_HIGH_SCHOOL_DIPLOMA_ONLY__2000',
+    #     'CI90LB517_2019', 'CI90LB017P_2019',
+    #     'UNEMPLOYED_2019', 'MED_HH_INCOME_PERCENT_OF_STATE_TOTAL_2019',
+    #     'MED_HH_INCOME_PERCENT_OF_STATE_TOTAL_2019',
+    #     'Metro_2013'],
     #     abs_corr_thresh=0.9
     # )
 
     # split train and test
     ss = SplitandScale(data_inputs_paths)
     X_train, X_test, y_train, y_test = ss.run_split_scale()
-    # modellisation
+    # modelling
     md = ModelData(X_train, X_test, y_train["TARGET"], y_test['TARGET'], model_config)
     md.run_modelling()
